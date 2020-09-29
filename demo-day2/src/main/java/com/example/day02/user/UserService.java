54833e219e650fdb1a13ca7214806f31e8da614f
up1 <somkiat.p@gmail.com>
2020-09-29 15:54:53 +0700

    Unit test with service

--- demo-day2/src/main/java/com/example/day02/user/UserService.java
+++ demo-day2/src/main/java/com/example/day02/user/UserService.java
@@ -8,8 +8,12 @@ import java.util.Optional;
 @Service
 public class UserService {
 
-    @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public UserDomain get(int id) {
         Optional<MyUser> result = userRepository.findById(id);
